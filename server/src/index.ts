3ffa25f81c9f755572a9b612a9e09a1244b16c45
yoog <907850084@qq.com>
2023-04-14 10:49:14 +0800

    feat: export ProjectManagerServer

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,10 @@ import { ExpressAdapter, NestExpressApplication } from "@nestjs/platform-express
 import { IoAdapter } from '@nestjs/platform-socket.io'
 import Options from "./options"
 import AppModule from "./module"
-import path from "path"
 
-async function bootstrap() {
-  const config = Options.default({
-    port: 4000,
-    db: path.resolve(process.cwd(), './config/app.db')
-  })
+export default async function ProjectManagerServer(...args: ConstructorParameters<typeof Options>) {
+  const config = Options.default(...args)
   const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter)
   app.useWebSocketAdapter(new IoAdapter(app))
   return await app.listen(config.port)
 }
-bootstrap()
