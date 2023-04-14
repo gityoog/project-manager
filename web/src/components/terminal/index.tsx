871b1fa0434e45fa9ec98f00f58a9cf73d9852da
yoog <907850084@qq.com>
2023-04-14 10:26:28 +0800

    feat: add ie scorllbar style

--- web/src/components/terminal/index.tsx
+++ web/src/components/terminal/index.tsx
@@ -1,5 +1,6 @@
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 import ResizeComponent from '../resize'
+import style from './style.module.scss'
 
 export interface iTerminal {
   open(el: HTMLElement): void
@@ -16,7 +17,7 @@ export default class TerminalComponent extends Vue {
     this.service.open(this.$el as HTMLElement)
   }
   protected render() {
-    return <ResizeComponent style="overflow: hidden" onResize={rect => {
+    return <ResizeComponent class={style.terminal} style="overflow: hidden" onResize={rect => {
       if (rect.height) {
         this.service.resize()
       }
