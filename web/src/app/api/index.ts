2829efeb42636d194cb9a4433888eb5fb3e13786
yoog <907850084@qq.com>
2023-04-14 15:24:06 +0800

    feat: add api

--- web/src/app/api/index.ts
+++ web/src/app/api/index.ts
@@ -1,9 +1,11 @@
 import project from "./project"
 import logging from "./logging"
+import config from "./config"
 
 const AppApi = {
   project,
-  logging
+  logging,
+  config: config
 }
 
 export default AppApi
