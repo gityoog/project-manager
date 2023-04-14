01a919ca86be0ff5f3a127511cc3b792e72896bb
yoog <907850084@qq.com>
2023-04-14 10:26:59 +0800

    feat: update config

--- web/script/index.ts
+++ web/script/index.ts
@@ -3,14 +3,23 @@ import path from 'path'
 const type = process.argv[2]
 
 const project = new WebProject({
-  outPath: path.resolve(__dirname, '../dist'),
+  outPath: path.resolve(__dirname, '../../server/dist/web'),
   context: path.resolve(__dirname, '../src'),
-  entry: {
-    app: './index.ts'
-  },
+  app: './index.ts',
+  polyfill: './polyfill/index.ts',
   analyzer: process.argv.some(arg => arg === '--analyzer'),
   proxyApi: 'http://localhost:4000',
   socketApi: 'ws://localhost:4000',
+  env: {
+    dev: {
+      mainApi: './api/',
+      socketApi: './socket/'
+    },
+    build: {
+      mainApi: './',
+      socketApi: './'
+    }
+  }
 })
 
 if (type === 'prod') {
