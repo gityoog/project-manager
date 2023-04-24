8494700db0f3f124e5ba3c8200c949c7cb60ce34
yoog <907850084@qq.com>
2023-04-24 11:46:40 +0800

    feat: add _

--- src/module/project/process/node-ipc/index.ts
+++ src/module/project/process/node-ipc/index.ts
@@ -11,7 +11,7 @@ export default class NodeIpcService {
     options: Options
   ) {
     const rand = Math.random().toString(36).substring(2, 15)
-    ipc.config.id = (options.isDev ? 'PROJECT_MANAGER_IPC_SERVER_DEV' : 'PROJECT_MANAGER_IPC_SERVER') + rand
+    ipc.config.id = (options.isDev ? 'PROJECT_MANAGER_IPC_SERVER_DEV' : 'PROJECT_MANAGER_IPC_SERVER') + '_' + rand
     ipc.config.retry = 1500
     ipc.config.logger = msg => logger.verbose(msg.replaceAll('\n', '\\n'), 'NodeIpcService')
     ipc.serve()
