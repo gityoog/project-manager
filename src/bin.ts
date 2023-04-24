ce1a2f347422a44b96c00a61af30e6a0ec46a788
yoog <907850084@qq.com>
2023-04-24 10:29:01 +0800

    feat: move bin

--- src/bin.ts
+++ src/bin.ts
@@ -1,4 +1,4 @@
-import ProjectManager from '..'
+import ProjectManager from '.'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import path from 'path'
@@ -12,11 +12,10 @@ yargs(hideBin(process.argv))
     command: '*',
     handler(data) {
       const web = path.resolve(__dirname, './web')
-      const web2 = path.resolve(__dirname, '../web')
       ProjectManager({
         port: data.port,
         db: path.resolve(process.cwd(), data.db),
-        web: fs.existsSync(web) ? web : fs.existsSync(web2) ? web2 : undefined
+        web: fs.existsSync(web) ? web : undefined
       })
     }
   })
