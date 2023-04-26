6f3746b35f3218e33cdaa5544174a8a4f324d885
yoog <907850084@qq.com>
2023-04-26 10:17:39 +0800

    feat: add port argv

--- packages/web/script/index.ts
+++ packages/web/script/index.ts
@@ -1,15 +1,16 @@
 import WebProject from "./web-project"
 import path from 'path'
 const type = process.argv[2]
-
+const arg3 = parseInt(process.argv[3])
+const port = isNaN(arg3) ? 3737 : arg3
 const project = new WebProject({
   outPath: path.resolve(__dirname, '../../../dist/web'),
   context: path.resolve(__dirname, '../src'),
   app: './index.ts',
   polyfill: './polyfill/index.ts',
   analyzer: process.argv.some(arg => arg === '--analyzer'),
-  proxyApi: 'http://localhost:3737',
-  socketApi: 'ws://localhost:3737',
+  proxyApi: 'http://localhost:' + port,
+  socketApi: 'ws://localhost:' + port,
   env: {
     dev: {
       mainApi: './api/',
--- src/dev.ts
+++ src/dev.ts
@@ -1,8 +1,10 @@
 import ProjectManagerServer from "."
 import path from "path"
 
+const port = parseInt(process.argv[2])
+
 ProjectManagerServer({
-  port: 3737,
+  port: isNaN(port) ? 3737 : port,
   db: path.resolve(process.cwd(), './config/app.db'),
   dev: true,
   web: path.resolve(__dirname, '../dist/web')
