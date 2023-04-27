6fdc9c67a74c19b483bdf9c542d76477c957ade1
yoog <907850084@qq.com>
2023-04-27 11:47:47 +0800

    feat: add bind and items

--- packages/web/src/common/local-storage-item/index.ts
+++ packages/web/src/common/local-storage-item/index.ts
@@ -9,8 +9,8 @@ function queryItem<T = any>(key: string): T | undefined {
   }
 }
 
-function setItem(key: string, value: any) {
-  localStorage.setItem(key, JSON.stringify(value))
+function setItem(key: string, value: any, noneStringify?: boolean) {
+  localStorage.setItem(key, noneStringify ? value : JSON.stringify(value))
 }
 
 export default class LocalStorageItem<T> {
@@ -43,35 +43,87 @@ export default class LocalStorageItem<T> {
     const localValue = queryItem(this.key)
     return localValue === undefined ? (isFn(this.def) ? this.def() : this.def) : localValue
   }
-  private changeCallback?: (value: T) => void
+  private changeCallback: Array<(value: T) => void> = []
   onChange(callback: (value: T) => void, immediate?: boolean) {
-    this.changeCallback = callback
+    this.changeCallback.push(callback)
     if (immediate) {
-      this.fireChange()
+      callback(this.value)
+    }
+    return () => {
+      const index = this.changeCallback.indexOf(callback)
+      if (index !== -1) {
+        this.changeCallback.splice(index, 1)
+      }
     }
   }
   private fireChange() {
-    this.changeCallback?.(this.value)
+    this.changeCallback.forEach(callback => callback(this.value))
   }
   get() {
     return this.value
   }
   set(value: T) {
-    if (this.value !== value) {
-      this.value = value
-      setItem(this.key, value)
+    if (this.value === value) return
+    var oldValue = JSON.stringify(this.value)
+    var newValue = JSON.stringify(value)
+    if (oldValue !== newValue) {
+      this.value = JSON.parse(newValue)
+      setItem(this.key, newValue, true)
       this.fireChange()
     }
   }
+  private binds: Array<{ obj: any, key: string }> = []
+  bind<K extends string>(obj: { [key in K]: T }, key: K): T {
+    this.binds.push({ obj, key })
+    Object.defineProperty(obj, key, {
+      get: () => this.get(),
+      set: (value) => {
+        this.set(value)
+      },
+    })
+    return this.get()
+  }
+
   clear() {
     localStorage.removeItem(this.key)
   }
   destroy() {
-    this.changeCallback = undefined
+    this.binds.forEach(({ obj, key }) => {
+      Object.defineProperty(obj, key, {
+        get: () => undefined,
+        set: () => { },
+      })
+    })
+    this.binds = undefined!
+    this.changeCallback = undefined!
     window.removeEventListener("storage", this.onStorageChange)
   }
 }
 
 function isFn(value: unknown): value is Function {
   return typeof value === 'function'
+}
+
+type Value<T extends LocalStorageItem<unknown>> = T extends LocalStorageItem<infer V> ? V : never
+
+export class LocalStorageItems<T extends {
+  [key: string]: LocalStorageItem<any>
+}> {
+  constructor(private data: T) {
+
+  }
+  watch = <K extends keyof T>(key: K, callback: (value: Value<T[K]>) => void, immediate?: boolean) => {
+    return this.data[key].onChange(callback, immediate)
+  }
+
+  bind<Key extends keyof T>(obj: { [key in Key]: Value<T[Key]> }, key: Key): Value<T[Key]>
+  bind<Key extends string, TK extends keyof T>(obj: { [key in Key]: Value<T[TK]> }, key: Key, prop: TK): Value<T[TK]>
+  bind<Key extends string, TK extends keyof T>(obj: { [key in Key]: Value<T[TK]> }, key: Key, prop?: TK): Value<T[TK]> {
+    return this.data[prop || key].bind(obj, key)
+  }
+
+  destroy() {
+    Object.values(this.data).forEach(item => item.destroy())
+    this.data = {} as T
+  }
 }
