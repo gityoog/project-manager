2129e65887a9b1d4cd9a6deb27cc20809e252443
yoog <907850084@qq.com>
2023-05-23 15:19:08 +0800

    feat: add encoding

--- src/common/pty/child_process/index.ts
+++ src/common/pty/child_process/index.ts
@@ -1,8 +1,16 @@
 import { spawn, ChildProcessWithoutNullStreams } from 'child_process'
 import ProcessKill from "@/common/utils/process"
+import iconv from 'iconv-lite'
 
 export default class ChildProcessService {
   private process?: ChildProcessWithoutNullStreams
+  constructor(private options: { encoding?: string } = {}) {
+    if (options.encoding) {
+      if (options.encoding === 'utf8' || !iconv.encodingExists(options.encoding)) {
+        options.encoding = undefined
+      }
+    }
+  }
   run({ shell, args, cwd, env, handler }: {
     shell: string
     args: string[]
@@ -23,10 +31,10 @@ export default class ChildProcessService {
       stdio: 'pipe'
     })
     this.process.stdout.on('data', data => {
-      handler.data(data.toString())
+      handler.data(this.decode(data, e => handler.error(e)))
     })
     this.process.stderr.on('data', data => {
-      handler.data(data.toString())
+      handler.data(this.decode(data, e => handler.error(e)))
     })
     this.process.on('error', (err) => {
       handler.error(err)
@@ -35,6 +43,16 @@ export default class ChildProcessService {
       handler.exit(exitCode, signal)
     })
     return this.process.pid!
+  }
+  private decode(data: Buffer, callback: (e: Error) => void) {
+    if (this.options.encoding) {
+      try {
+        return iconv.decode(data, this.options.encoding)
+      } catch (e) {
+        callback(e instanceof Error ? e : Error('iconv.decode error'))
+      }
+    }
+    return data.toString()
   }
   stop() {
     if (this.process) {
