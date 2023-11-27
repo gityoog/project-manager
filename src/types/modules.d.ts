377693a1f18c9e175c72ffbf0b598945f7195023
yoog <907850084@qq.com>
2023-11-27 14:06:09 +0800

    feat: remove process-tree

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -24,17 +24,6 @@ declare module '@achrinza/node-ipc' {
   export = RootIPC
 }
 
-declare module 'process-tree' {
-  type Tree<T> = T & {
-    children: Tree<T>[]
-  }
-  export default function (pid: number, callback: (err: Error | null, children: Tree<{
-    pid: number
-    ppid: number
-    name: number
-  }>[]) => void): void
-}
-
 declare module 'lodash.debounce' {
   export default function debounce(fn: Function, delay: number): Function
 }
