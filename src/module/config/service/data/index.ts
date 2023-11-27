817819f13bbd0882fa2308fd45b2672548ade876
yoog <907850084@qq.com>
2023-11-27 16:44:52 +0800

    feat: remove import

--- src/module/config/service/data/index.ts
+++ src/module/config/service/data/index.ts
@@ -3,7 +3,6 @@ import { InjectRepository } from "@nestjs/typeorm"
 import { Repository } from 'typeorm'
 import os from 'os'
 import ConfigEntity from "../entity"
-import LoggingService from "@/module/logging/service"
 import PtyService from "@/common/pty"
 
 type Item<T = string> = {
