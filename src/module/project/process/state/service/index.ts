b49d87140183a54e8d20ad5790a81eaa15132e3c
yoog <907850084@qq.com>
2023-11-28 10:17:38 +0800

    feat: add queue

--- src/module/project/process/state/service/index.ts
+++ src/module/project/process/state/service/index.ts
@@ -5,6 +5,8 @@ import ProjectProcessStateEntity from "./entity"
 import ProjectProcessBus from "../../bus"
 import ProjectService from "../../../service"
 import ProjectProcessService from "../../service"
+import ConfigService from "@/module/config/service"
+import CacheQueue from "@/common/cache-queue"
 
 @Injectable()
 export default class ProjectProcessStateService {
@@ -13,18 +15,35 @@ export default class ProjectProcessStateService {
     private bus: ProjectProcessBus,
     private project: ProjectService,
     private process: ProjectProcessService,
-    private logger: Logger
+    private logger: Logger,
+    private config: ConfigService
   ) {
     this.init()
   }
   private async init() {
     this.bus.on(data => {
       if (data.action === 'status') {
-        this.addCache(data.id, data.value)
+        this.queue.add({
+          id: data.id,
+          status: data.value
+        })
       }
     })
-  }
+    this.queue.pull(async data => {
+      const value = data.reduce((p, c) => (p[c.id] = c.status, p), {} as Record<string, boolean>)
+      await this.write(value)
+    })
 
+    const data = await this.project.getAllProcess()
+    const keepProcess = await this.config.getKeepProcess()
+
+    for (const { process, id, name } of data) {
+      if (process.autostart || (keepProcess && await this.status(process.id))) {
+        const pid = await this.process.run(id, process.id)
+        this.logger.log(`Autostart ${name}[${process.id}]: ${pid}`, 'ProjectProcessStateService')
+      }
+    }
+  }
   async status(id: string) {
     const row = await this.main.findOneBy({ id })
     if (row) {
@@ -32,43 +51,21 @@ export default class ProjectProcessStateService {
     }
     return false
   }
+  private queue = new CacheQueue<{
+    id: string
+    status: boolean
+  }>()
 
-  private cache: Record<string, boolean> | null = null
-  private addCache(id: string, status: boolean) {
-    if (this.cache === null) {
-      this.cache = {}
-    }
-    this.cache[id] = status
-    this.addUpdater()
-  }
-  private updater: NodeJS.Timeout | null = null
-  private lock = false
-  private addUpdater() {
-    if (this.lock) return
-    if (!this.cache) return
-    if (this.updater) {
-      clearTimeout(this.updater)
-    }
-    this.updater = setTimeout(() => {
-      this.flush()
-    }, 3000)
-  }
-  private async flush() {
-    if (this.cache) {
-      const data = this.cache
-      this.cache = null
-      this.lock = true
-      for (const id in data) {
-        const value = data[id]
-        const row = await this.main.findOneBy({ id })
-        if (row) {
-          await this.main.save(this.main.merge(row, { status: value }))
-        } else {
-          await this.main.save(this.main.create({ id, status: value }))
-        }
+  private async write(data: Record<string, boolean>) {
+    for (const id in data) {
+      const value = data[id]
+      const row = await this.main.findOneBy({ id })
+      if (row) {
+        await this.main.save(this.main.merge(row, { status: value }))
+      } else {
+        await this.main.save(this.main.create({ id, status: value }))
       }
-      this.lock = false
-      this.addUpdater()
     }
+    this.logger.debug(`Write state: ${JSON.stringify(data)}`, 'ProjectProcessStateService')
   }
 }
