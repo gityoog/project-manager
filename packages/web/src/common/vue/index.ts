cf23b40795d00aa8caf7ecfa60f2e6d3055fc3c7
yoog <907850084@qq.com>
2023-11-29 14:34:22 +0800

    feat: add ToFC

--- packages/web/src/common/vue/index.ts
+++ packages/web/src/common/vue/index.ts
@@ -1,6 +1,10 @@
-import { FunctionalComponentOptions } from 'vue'
+import { FunctionalComponentOptions, Component } from 'vue'
 
-export function FC<T extends object>(component: FunctionalComponentOptions<T>) {
+export function FC<T extends object>(component: FunctionalComponentOptions<T> | NonNullable<FunctionalComponentOptions<T>['render']>) {
+  if (typeof component === 'function') {
+    component = { render: component, functional: true }
+  }
+  component.functional = true
   return component as unknown as Tsx.Component<T>
 }
 
@@ -10,4 +14,8 @@ export function FCGeneric<T extends ((props: any) => JSX.Element)>(component: Fu
 
 export function GenericComponent<T extends ((props: any) => JSX.Element)>(component: any) {
   return component as unknown as Tsx.Component<Parameters<T>[0]>
+}
+
+export function ToFC<T extends object>(component: Tsx.Component<T>, props: T) {
+  return FC<{}>(h => h(component as unknown as Component, { props }))
 }
