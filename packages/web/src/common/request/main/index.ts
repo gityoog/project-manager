076850529b6c539c5356f7c552bb8ddbe0e8ab33
yoog <907850084@qq.com>
2024-01-16 11:09:12 +0800

    feat: add NoAuthHandler

--- packages/web/src/common/request/main/index.ts
+++ packages/web/src/common/request/main/index.ts
@@ -24,6 +24,7 @@ interface MainRequest {
   url<T = void>(generate: (data: T) => string): (data: T) => string
   bindBaseUrl: (url: string) => void
   bindErrorhandler(callback: (error: string) => void): void
+  bindNoAuthHandler(callback: () => void): void
   bindToken: (token: string) => void
 }
 
@@ -33,6 +34,9 @@ type Response<T> = {
 } | {
   status: 1
   info: T
+} | {
+  status: -1
+  msg: string
 }
 
 const MainRequest: MainRequest = <T = void, R = void>(config: AxiosRequestConfig) => WRequest.Build<T, R>(params => service({
@@ -44,6 +48,9 @@ const MainRequest: MainRequest = <T = void, R = void>(config: AxiosRequestConfig
   if (data && 'status' in data) {
     if (data.status === 0) {
       throw new Error(data.msg)
+    } else if (data.status === -1) {
+      noAuthHandler()
+      throw new Error(data.msg)
     } else {
       return data.info
     }
@@ -52,6 +59,7 @@ const MainRequest: MainRequest = <T = void, R = void>(config: AxiosRequestConfig
 })).handle(wRequest => wRequest.fail(errorHandler))
 
 let errorHandler = (error: string) => { console.error(error) }
+let noAuthHandler = () => { console.error('no auth') }
 
 MainRequest.url = callback => data => {
   const url = callback(data)
@@ -68,5 +76,6 @@ MainRequest.page = (config) => MainRequest(config)
 MainRequest.bindBaseUrl = url => service.defaults.baseURL = url
 MainRequest.bindErrorhandler = callback => errorHandler = callback
 MainRequest.bindToken = token => service.defaults.headers['Token'] = token
+MainRequest.bindNoAuthHandler = callback => noAuthHandler = callback
 
 export default MainRequest
